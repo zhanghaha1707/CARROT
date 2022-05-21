4ed70412b7db7d619b5828e5d700ca1574204f69
“zhanghaha1707” <zhangyuixn@163.com>
2022-05-21 11:41:47 +0800

    git push -u origin main

--- preprocess-lstm/main.py
+++ preprocess-lstm/main.py
@@ -1,3 +1,11 @@
+'''
+Author: “zhanghaha1707” zhangyuixn@163.com
+Date: 2022-05-21 09:50:59
+LastEditors: “zhanghaha1707” zhangyuixn@163.com
+LastEditTime: 2022-05-21 10:11:31
+FilePath: /CARROT_/preprocess-lstm/main.py
+Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+'''
 # -*- coding: utf-8 -*-
 """
 Created on Tue Mar  3 17:57:54 2020
@@ -17,7 +25,12 @@ def dataset(dir = './tmp', tgt = '../data/oj.pkl.gz',
             symtab = '../data/oj_uid.pkl.gz', 
             inspos_file = '../data/oj_inspos.pkl.gz',
             done_file = 'dataset.done'):
+    """处理数据集的函数
     
+    args:
+        dir:暂时存放中间数据的位置
+        tgt:
+    """
     if tk.unzip():
         d = tk.tokenize()
         if d is not None:
